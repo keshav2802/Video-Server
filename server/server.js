85bc75910eefa89422ec3e37926eceb1008dea8e
keshavism28 <keshavism28@gmail.com>
2020-09-29 21:43:01 +0530

    setup the dependencies in the server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('App is running');
-})
+// Used to log everything like GET, POST, etc requests
+app.use(morgan('dev'));
+
+// It ensures that we prevent Cross-Origin Resource Sharing(CORS) errors
+// If client makes a request from localhost:3000, and received a response from 
+// server which has localhost:5000, req will fail. It is always the case with 
+// RESTful APIs. So, we attach headers in the response from servers to client 
+// to tell the browser that it's OK
+app.use(cors());
+
+// body-parser extracts json data and makes it easily readable to us.
+app.use(bodyParser.json());
+
+// extended: true allows to parse extended body with rich data in it.
+// extended: false means the client can send only simple json objects
+// and not nested objects.
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 app.use('/api/signup', require('./routes/signup'));
 
