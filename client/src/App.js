135da7f0fe6ac1fe3f051dcf90bfee5305bfc373
keshavism28 <keshavism28@gmail.com>
2020-09-30 02:02:58 +0530

    created the signup component for the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import SignIn from './components/Form/SignIn/SignIn';
+import SignUp from './components/Form/SignUp/SignUp';
 
 function App() {
   return (
-    <Route exact path="/signin" component={SignIn} />
+    <React.Fragment>
+      <Route exact path="/signin" component={SignIn} />
+      <Route exact path="/signup" component={SignUp} />
+    </React.Fragment>
   );
 }
 
