2442e83b773c7a3809b74d9e1d698447c7df8550
keshavism28 <keshavism28@gmail.com>
2020-09-30 12:23:07 +0530

    crated the Upload component for uploading a video

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import SignIn from './components/Form/SignIn/SignIn';
 import SignUp from './components/Form/SignUp/SignUp';
+import Upload from './components/Upload/Upload';
 
 function App() {
   return (
     <React.Fragment>
       <Route exact path="/signin" component={SignIn} />
       <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/upload" component={Upload} />
     </React.Fragment>
   );
 }
